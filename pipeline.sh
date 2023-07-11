ec68ebd205cf4d7f85788d9d9822e4d14a76738a
aman deep <aman.deep@uni-due.de>
2023-07-11 08:07:06 +0000

    first commit

--- pipeline.sh
+++ pipeline.sh
@@ -11,5 +11,5 @@ conda activate natrix
 varname+=".yaml"
 cores=$(grep "cores : " $varname | awk '{print $3}')
 if python create_dataframe.py "$varname"; then
-    screen -S $varname bash -c "source $env_loc;conda activate natrix;snakemake --use-conda --cores $cores --configfile $varname -p -r  -n -F ; exec sh"
+    screen -S $varname bash -c "source $env_loc;conda activate natrix;snakemake --use-conda --cores $cores --configfile $varname -p -r  ; exec sh"
 fi
