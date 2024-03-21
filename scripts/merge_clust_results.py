0ff7b06e3b6cf29997258cbacdcdb4b7f79c90f4
Dana Bludau <108586018+DanaBlu@users.noreply.github.com>
2024-03-21 11:22:50 +0100

    Removal of duplicate sequences

--- scripts/merge_clust_results.py
+++ scripts/merge_clust_results.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import collections as col
 
-filtered_dict = pd.read_csv(snakemake.input["final_table_path2"],
-                index_col="seqid").to_dict(orient="index")
+filtered_dict = pd.read_csv(snakemake.input["final_table_path2"], index_col="seqid").to_dict(orient="index")
 
 with open(snakemake.input["merged"], "r") as swarms:
     seq_names = []
@@ -12,14 +11,14 @@ with open(snakemake.input["merged"], "r") as swarms:
 
 swarm_dict = col.defaultdict()
 for j in range(len(seq_names)):
-    swarm_dict[j] = {}
-    swarm_dict[j]["sequences"] = filtered_dict[">" + seq_names[j][0]]["sequences"]
-    for sample in filter(lambda i: i not in ["sequences"], filtered_dict[">"
-                    + seq_names[j][0]].keys()):
-        swarm_dict[j][sample] = sum([filtered_dict[">"
-            + key][sample] for key in seq_names[j]])
-    swarm_dict[j]["seqid"] = "N{}_{}".format(seq_names[j][0].split(";")[0],
-        sum([value for key, value in swarm_dict[j].items() if not key in {"seqid", "sequences"}]))
+    seq_id = ">" + seq_names[j][0]
+    if seq_id in filtered_dict:  # Check if sequence ID exists in filtered_dict
+        swarm_dict[seq_id] = {}
+        swarm_dict[seq_id]["sequences"] = filtered_dict[seq_id]["sequences"]
+        for sample in filter(lambda i: i != "sequences", filtered_dict[seq_id].keys()):  # Iterate over samples
+            swarm_dict[seq_id][sample] = sum([filtered_dict[">" + key][sample] for key in seq_names[j]])
+        swarm_dict[seq_id]["seqid"] = "N{}_{}".format(seq_names[j][0].split(";")[0],
+                                                      sum([value for key, value in swarm_dict[seq_id].items() if key not in {"seqid", "sequences"}]))
 
 df = pd.DataFrame.from_dict(swarm_dict, orient="index").set_index("seqid")
 df.to_csv(snakemake.output['out'])
